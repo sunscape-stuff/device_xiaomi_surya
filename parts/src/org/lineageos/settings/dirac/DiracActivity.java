ae64a2addacec9836d9f58f2cec4aad96f3dd8a4
AnierinB <anierin@evolution-x.org>
2024-01-20 16:34:52 +0900

    surya: Move to PreferenceFragmentCompat
    PreferenceFragment was deprecated in API level 28.
    
    https://developer.android.com/jetpack/androidx/releases/preference#1.1.0
    Signed-off-by: David112x <86davidx64@gmail.com>

--- parts/src/org/lineageos/settings/dirac/DiracActivity.java
+++ parts/src/org/lineageos/settings/dirac/DiracActivity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018-2022 The LineageOS Project
+ * Copyright (C) 2018-2024 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,7 +28,7 @@ public class DiracActivity extends CollapsingToolbarBaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getFragmentManager().beginTransaction().replace(com.android.settingslib.collapsingtoolbar.R.id.content_frame,
+        getSupportFragmentManager().beginTransaction().replace(com.android.settingslib.collapsingtoolbar.R.id.content_frame,
                 new DiracSettingsFragment(), TAG_DIRAC).commit();
     }
 }
